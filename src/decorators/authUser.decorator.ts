83f55dfb65852f390f058e7ba6817a6d01a4cce3
nazarii <nazarii.honcharenko@haven.com>
2024-11-01 00:16:35 +0200

    Add AuthUser

---
+++ src/decorators/authUser.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator } from '@nestjs/common';
+
+export const AuthUser = createParamDecorator(
+    (data, ctx) => {
+        const request = ctx.switchToHttp().getRequest();
+        return request.user;
+    },
+);
